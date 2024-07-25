16026465e74ffc94c6a6b7c2ac47a961defb4332
bharath hs <bharathhs@bharaths-MacBook-Pro.local>
2024-07-25 07:03:13 -0700

    V4:Education Completed

--- src/components/V4Main/V4Main.jsx
+++ src/components/V4Main/V4Main.jsx
@@ -4,8 +4,11 @@ import React, { useEffect } from 'react';
 import V4Header from '../V4Header/V4Header';
 import V4Bio from '../V4Bio/V4Bio';
 import V4Cursor from '../V4Cursor/V4Cursor';
+import V4Contact from '../V4Contact/V4Contact';
 import V4Skills from '../V4Skills/V4Skills';
 import V4Experience from '../V4Experience/V4Experience';
+import V4Education from '../V4Education/V4Education';
+import V4Footer from '../V4Footer/V4Footer';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
@@ -22,6 +25,9 @@ const V4Main = () => {
         <V4Bio />
         <V4Skills />
         <V4Experience />
+        <V4Education />
+        <V4Contact />
+        <V4Footer />
       </div>
     </>
   );
